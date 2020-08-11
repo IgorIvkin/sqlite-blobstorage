53fb818135723b2297bc2bd196ba1928a7386c89
unknown <group06vp1@yandex.ru>
2020-08-11 18:53:02 +0300

    exception handling refactoring

--- src/main/java/com/igorivkin/blobstorage/exceptions/GenericDatabaseException.java
+++ src/main/java/com/igorivkin/blobstorage/exceptions/GenericDatabaseException.java
@@ -1,7 +1,2 @@
-package com.igorivkin.blobstorage.exceptions;
-
-public class GenericDatabaseException extends Exception {
-    public GenericDatabaseException(String message) {
-        super(message);
-    }
+package com.igorivkin.blobstorage.exceptions;public class GenericDatabaseException {
 }
